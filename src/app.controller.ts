5a98460b39ae8cff34481d4db5d355188b28140c
성환 <psung9510@gmail.com>
2023-12-27 15:38:37 +0900

    Feat: CI/CD Test6

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
 
   @Get()
   home() {
-    return 'Production Server(Feat.Docker123456789)';
+    return 'Production Server(Feat.Docker12345678900000)';
   }
 
   @Get('health-check')
